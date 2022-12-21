d0dbc127fd713f504ee584a38796b3fc7299276c
Bismark Amuna <bismarkamuna007@gmail.com>
2022-12-21 01:18:18 +0000

    5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -7,7 +7,7 @@ class Square:
     """Represent a square."""
 
     def __init__(self, size):
-    """Initialize a new square.
+        """Initialize a new square.
 
         Args:
             size (int): The size of the new square.
